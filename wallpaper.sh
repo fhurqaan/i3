c6d9f746e0f13659fa58a28ff0649f814eacdb8c
Fhurqaan Hamid <git@github.com/fhurqaan/i3.git>
2022-07-24 14:08:57 +0800

    set wallpaper

--- wallpaper.sh
+++ wallpaper.sh
@@ -6,7 +6,7 @@ pgrep sxiv;retval=$?
 if [ $retval -eq 1 ] && [ -f ~/.local/Sxiv.lock ] ; then
 	rm ~/.local/Sxiv.lock
 fi
-[ -f ~/.local/Sxiv.lock ] && kdialog --error  "Sxiv instance already running..." --title "SXIV error message" && exit
+[ -f ~/.local/Sxiv.lock ] && kdialog --error  "Sxiv instance already running..." --title "SXIV error !" && exit
 touch ~/.local/Sxiv.lock
 case $1 in
     new) sxiv -qtrg 480x320 ~/Pictures/* ;;
