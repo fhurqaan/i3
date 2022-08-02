c3857115e99eee09746f93fc1edbc835aeb3648e
Fhurqaan Hamid <git@github.com/fhurqaan/i3.git>
2022-08-02 18:44:58 +0800

    Set wallpaper

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-#ext="${wpf:(-4)}"
-# check is unexpected shutdown and lock file not removed
-# then remove lock file.
-pgrep sxiv;retval=$? 
-if [ $retval -eq 1 ] && [ -f ~/.local/Sxiv.lock ] ; then
-	rm ~/.local/Sxiv.lock
-fi
-[ -f ~/.local/Sxiv.lock ] && kdialog --error  "Sxiv instance already running..." --title "SXIV error !" && exit
-touch ~/.local/Sxiv.lock
+# only allow one instance of sxiv to run
+pidof /usr/bin/sxiv > /dev/null && {
+	kdialog --error  "Sxiv instance already running..." --title "SXIV error"
+	exit
+}
 case $1 in
     new) sxiv -qtrg 480x320 ~/Pictures/* ;;
     set) ~/.fehbg & ;;
 esac
-rm ~/.local/Sxiv.lock
